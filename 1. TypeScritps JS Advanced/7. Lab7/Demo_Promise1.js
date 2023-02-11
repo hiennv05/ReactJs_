fbcb50311b893d38027166ff1eca014b798c2d83
hiennv05 <bg.hien@gmail.com>
2023-02-11 23:17:51 +0900

    Demo Promise

---
+++ 1. TypeScritps JS Advanced/7. Lab7/Demo_Promise1.js
@@ -0,0 +1,57 @@
+// Check email
+function callAPICheckEmail() {
+    // @ts-ignore
+    var promiseCheckEmail = new Promise(function (resolve, reject) {
+        console.log("---Bước 1: Check email");
+        var resultEmail = true;
+        if (resultEmail) {
+            reject("This is Email Exists");
+        }
+        else {
+            resolve("Check Email Ok!");
+        }
+    });
+    return promiseCheckEmail;
+}
+// Check Username
+function callAPICheckUsername() {
+    // @ts-ignore
+    return new Promise(function (resolve, reject) {
+        console.log("---Bước 2: Call API check username");
+        var data = false;
+        if (data) {
+            reject("This username is Exists!");
+        }
+        else {
+            resolve("Check Username ok");
+        }
+    });
+}
+// Creat Account
+function callAPICreatAccount() {
+    // @ts-ignore
+    return new Promise(function (resolve, reject) {
+        console.log("--- Bước 3: Call API creat account");
+        var resultCreat = true;
+        if (resultCreat) {
+            resolve("Creat Account success!");
+        }
+        else {
+            reject("An error has occurred!");
+        }
+    });
+}
+// Use
+callAPICheckEmail()
+    .then(function (msgSuccess) {
+    console.log(msgSuccess);
+    return callAPICheckUsername();
+}).then(function (msgSuccess) {
+    console.log(msgSuccess);
+    return callAPICreatAccount();
+}).then(function () {
+    console.log("Tạo mới thành công");
+})["catch"](function (msgError) {
+    console.log("Error");
+    console.log(msgError);
+});
